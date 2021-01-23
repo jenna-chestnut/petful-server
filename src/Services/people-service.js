827a41b342afdb819143bae3b4a8f08a3c78ed7a
Jenna Chestnut <jennabot5000@gmail.com>
2021-01-23 14:08:53 -0600

    queue random people if needed

--- src/Services/people-service.js
+++ src/Services/people-service.js
@@ -7,16 +7,18 @@ people.forEach(el => {
   PeopleQ.enqueue(el);
 });
 
-const fillerNames = ['Babby', 'Artia', 'Borksy', 'Macaroni', 'Digital', 'Essie', 'Green', 'Pinny', 'Dollah'];
+const fillerNames = ['Babby', 'Artia', 'Borksy', 'Macaroni', 'Digital', 'Essie', 'Green', 'Pinny', 'Dollah', 'Tuesday', 'August', 'Sunny', 'Juniper', 
+  'Cascada', 'Cheddar'];
 
 const all = () => {
   let peopleList = PeopleQ.all();
 
-  if (peopleList.length === 0) {
+  if (peopleList.length === 1) {
+    const rName = () => { 
+      return fillerNames[Math.floor(Math.random() * fillerNames.length)]; 
+    };
     let random = [
-      fillerNames[Math.floor(Math.random() * 9)],
-      fillerNames[Math.floor(Math.random() * 9)],
-      fillerNames[Math.floor(Math.random() * 9)]
+      rName(), rName(), rName()
     ];
 
     random.forEach(el => {
