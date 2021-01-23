dbffb1a2ba84417569a8f7026720937400f24412
Jenna Chestnut <jennabot5000@gmail.com>
2021-01-23 14:47:57 -0600

    added more to tests

--- test/people-endpoints.test.js
+++ test/people-endpoints.test.js
@@ -2,20 +2,19 @@
 const { expect } = require('chai');
 const app = require('../src/app');
 
-describe.only('People Endpoints', () => {
+describe('People Endpoints', () => {
   it('GET /people responds with 200 containing list of people in line to adopt', () => {
     return supertest(app).get('/api/people').expect(200)
       .then(res => {
-        console.log(res.body);
         expect(res.body).to.be.an('array');
+        expect(res.body.length).to.be.greaterThan(0);
       });
   });
   it('POST /people adds new person name to list of people in line to adopt', () => {
-    let newP = {name : 'Felicia Bai'};
+    let newP = { name : 'Felicia Bai' };
     return supertest(app).post('/api/people').expect(201)
       .send(newP)
       .then(res => {
-        console.log(res.body);
         expect(res.body).to.be.a('string');
         expect(res.body).to.eql(newP.name);
       });
--- test/pet-endpoints.test.js
+++ test/pet-endpoints.test.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-undef */
+const { expect } = require('chai');
 const app = require('../src/app');
 
 describe('Pet Endpoints', () => {
@@ -6,14 +7,17 @@ describe('Pet Endpoints', () => {
     return supertest(app).get('/api/pets').expect(200)
       .then(res => {
         expect(res.body).to.be.an('object');
+        expect(res.body.cat).to.be.an('object');
+        expect(res.body.dog).to.be.an('object');
       });
   });
   it('DELETE /pet removes selected pet from line and their owner, returning the new family!', () => {
     return supertest(app).delete('/api/pets/cat')
       .expect(200)
       .then(res => {
-        console.log(res.body);
         expect(res.body).to.be.an('object');
+        expect(res.body.pet).to.be.an('object');
+        expect(res.body.owner).to.be.a('string');
       });
   });
 });
