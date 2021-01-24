0fcdf180376b070daf9d8cffa0b1e43502392c66
Jenna Chestnut <jennabot5000@gmail.com>
2021-01-23 19:48:44 -0600

    tweak people service for more people added

--- src/Services/people-service.js
+++ src/Services/people-service.js
@@ -7,16 +7,16 @@ people.forEach(el => {
   PeopleQ.enqueue(el);
 });
 
-const fillerNames = ['Babby', 'Artia', 'Borksy', 'Macaroni', 'Digital', 'Essie', 'Green', 'Pinny', 'Dollah', 'Tuesday', 'August', 'Sunny', 'Juniper', 
-  'Cascada', 'Cheddar'];
+const fillerNames = ['Babby', 'Artia', 'Borksy', 'Macaroni', 'Digital', 'Essie', 'Green', 'Pinny', 'Dollah', 'Tuesday', 'August', 'Sunny', 'Juniper', 'Cascada', 'Cheddar'];
 
 const all = () => {
   let peopleList = PeopleQ.all();
 
   if (peopleList.length === 0) {
-    PeopleQ.enqueue(
+    const pplQ = () => PeopleQ.enqueue(
       fillerNames[Math.floor(Math.random() * fillerNames.length)]
     );
+    pplQ(); pplQ();
   }
 
   return PeopleQ.all();
